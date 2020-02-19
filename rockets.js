9ec39d565737433ff3c8069bf3fbf8c78826ac3a
rachel fischoff <59700665+rachel-fischoff@users.noreply.github.com>
2020-02-19 14:52:09 -0500

    with secondary engines

--- rockets.js
+++ rockets.js
@@ -4,6 +4,11 @@ var Rocket = function (color, size) {
 
   this.launch = function () {
     console.log('3, 2, 1, blastoff!');
+
+    this.engageSecondaryEngines  = function () {
+      console.log('Secondary Engines Engaged!');
+    };
+    
   }
 }
 
